dc2bea09dceb976be7ab4d920fd23f162f9f94b3
Beth Ann Shadwick <beth.shadwick@smail.astate.edu>
2020-05-05 11:55:10 -0500

    Changes

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 var displayedImage = document.querySelector('.displayed-img');
 var thumbBar = document.querySelector('.thumb-bar');
 
+var btn = document.querySelector('button');
+var overlay = document.querySelector('.overlay');
 
-/* Looping through images */
-
+for(let i = 1; i <= 5; i++) {
   var newImage = document.createElement('img');
-  newImage.setAttribute('src', xxx);
+  newImage.setAttribute('src', 'images/pic' + i + '.jpg');
   thumbBar.appendChild(newImage);
+  newImage.onclick = function(e) {
+    displayedImage.src = e.target.src;
+  }
+}
+
+btn.onclick = function() {
+  var btnClass = btn.getAttribute('class');
+  if(btnClass === 'dark') {
+    btn.setAttribute('class', 'light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(197, 84, 34, 0.53)';
+  }
+}
