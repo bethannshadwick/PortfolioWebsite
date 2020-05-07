69cfcb284235bfa84128e95e5256d31a1afc5021
Beth Ann Shadwick <beth.shadwick@smail.astate.edu>
2020-05-07 14:46:23 -0500

    that didn't work

--- main.js
+++ main.js
@@ -16,20 +16,6 @@ for(let i = 1; i <= 5; i++) {
   }
 }
 
-var myIndex = 0;
-carousel();
-
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-myIndex++;
-if (myIndex > x.length) {myIndex = 1}
-x[myIndex-1].style.display = "block";
-setTimeout(carousel, 3000);
-}
 
 btn.onclick = function() {
   var btnClass = btn.getAttribute('class');
