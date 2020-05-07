69797c56591a58560bfeb0a12985e9978d0c48e2
Beth Ann Shadwick <beth.shadwick@smail.astate.edu>
2020-05-07 14:02:42 -0500

    loop

--- main.js
+++ main.js
@@ -16,6 +16,18 @@ for(let i = 1; i <= 5; i++) {
   }
 }
 
+(function(){
+  var rotator= document.getElementById('rotator'),
+  dir='images/',
+  delayInSeconds=1,
+  num= 0;
+  len= N;
+  setInterval(function(){
+    rotator.src = dir + num + '.jpg';
+    num = (num === len) ? 0 : ++num; },
+    delayInSeconds * 50);
+}
+
 btn.onclick = function() {
   var btnClass = btn.getAttribute('class');
   if(btnClass === 'dark') {
